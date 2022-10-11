e40f324b32dd78b64358f41780c42c162f648490
Kusuma sri <111426103+KusumaSri28@users.noreply.github.com>
2022-10-11 15:40:56 +0530

    m=int(input()) for i in range(m):     n=int(input())     np=n     while True:         fc=0         for i in range(1,np+1):           if np%i==0:             fc+=1         if fc==2:          break         np-=1     cp=n     while True:         fc=0         for i in range(1,cp+1):           if cp%i==0:             fc+=1         if fc==2:          break         cp+=1     x=cp-n     y=n-np     if x>y:         print(np)     elif x<y:         print(cp)     elif x==y:         print(min(np,cp))

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,23 +1,23 @@
 m=int(input())
-for _ in range(m):
+for i in range(m):
     n=int(input())
     np=n
     while True:
         fc=0
         for i in range(1,np+1):
-            if np%i==0:
-                fc+=1
+          if np%i==0:
+            fc+=1
         if fc==2:
-            break
+         break
         np-=1
     cp=n
     while True:
         fc=0
         for i in range(1,cp+1):
-            if cp%i==0:
-                fc+=1
+          if cp%i==0:
+            fc+=1
         if fc==2:
-            break
+         break
         cp+=1
     x=cp-n
     y=n-np
@@ -27,5 +27,3 @@ for _ in range(m):
         print(cp)
     elif x==y:
         print(min(np,cp))
-    
-
