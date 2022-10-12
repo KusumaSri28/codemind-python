28c265bfccc9bea86bc8ce122a773fd8d1b20841
Kusuma sri <111426103+KusumaSri28@users.noreply.github.com>
2022-10-12 19:33:20 +0530

    m,n=map(int,input().split()) for i in range(1,m+1):     if m==0 or n==0:         break     elif m>n:         m%=n     else:         n%=m if m==0:     print(n) else:     print(m)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,12 +1,12 @@
 m,n=map(int,input().split())
-while 1:
+for i in range(1,m+1):
     if m==0 or n==0:
         break
-    if m>n:
+    elif m>n:
         m%=n
     else:
         n%=m
-if n==0:
-    print(m)
-else:
+if m==0:
     print(n)
+else:
+    print(m)
